56e3917dd97409f349a2fdf8c0bfbdab2766e42e
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-10-30 16:54:04 -0600

    including mongodb assignment

--- app.js
+++ app.js
@@ -1,52 +1,18 @@
 const express = require('express');
-const session = require('express-session')
-const bodyParser = require('body-parser');
 const app = express();
-const router = express.Router();
+const port = process.env.PORT || 3000;
+const mongodb = require('./data/database')
 
-app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', require('./routes'));
 
-var sess;
 
-router.get('/', (req, res) => {
-  sess = req.session;
-  if (sess.email) {
-    return res.redirect('/admin');
-  }
-  res.send('Ok');
-});
- 
-router.post('/login', (req, res) => {
-  sess = req.session;
-  sess.email = req.body.email;
-  res.end('done');
-});
- 
-router.get('/admin', (req, res) => {
-  sess = req.session;
-  if (sess.email) {
-    res.write(`Hello ${sess.email}`);
-    res.end('Logout');
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err)
   } else {
-    res.write('Please login first.');
-    res.end('Login');
+    app.listen(port, () => {
+      console.log(`Database is Listening Running on port ${port}`)
+    })
   }
 });
- 
-router.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return console.log(err);
-    }
-    res.redirect('/');
-  });
-});
 
-
-app.use('/', router)
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Web server is listening at port ' + (process.env.PORT || 3000));
-});
