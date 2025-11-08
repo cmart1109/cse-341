dd808f1551af31ba335fadbe379d64dd0a7bb811
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-11-08 16:58:08 -0600

    swagger

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const mongodb = require('./data/database');
 
 
 app.use(bodyParser.json());
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  next();
+});
 app.use('/', require('./routes/index'));
 app.use('/contacts', require('./routes/contacts'))
 
